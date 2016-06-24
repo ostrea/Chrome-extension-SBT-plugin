2118a8ee03b37f010a2d37e5cb643225664491a2
Ostrea <Ostrea@users.noreply.github.com>
2016-06-24 15:47:40 +0300

    Copy resources and js file to the created folder

--- src/main/scala/my/ostrea/chromeextensionsbtplugin/ChromeExtensionSbtPlugin.scala
+++ src/main/scala/my/ostrea/chromeextensionsbtplugin/ChromeExtensionSbtPlugin.scala
@@ -1,10 +1,13 @@
 package my.ostrea.chromeextensionsbtplugin
 
-import java.nio.file.Paths
+import java.nio.file.{DirectoryStream, Files, Path, Paths}
+import java.io.{File, IOException}
 
 import org.scalajs.sbtplugin.ScalaJSPlugin
 import sbt.Keys._
 import sbt.{AutoPlugin, Compile, IO, Setting, taskKey}
+
+import scala.collection.JavaConversions._
 
 object ChromeExtensionSbtPlugin extends AutoPlugin {
 
@@ -21,16 +24,36 @@ object ChromeExtensionSbtPlugin extends AutoPlugin {
   def createUnpackedExtensionSetting: Setting[_] = createUnpackedExtension := {
     val log = streams.value.log
 
-    log.info("JS generated.")
     (ScalaJSPlugin.autoImport.fullOptJS in Compile).value
+    log.info("JS generated.")
 
-    createDirectoryForExtension
-  }
+//    createDirectoryForExtension()
 
-  private def createDirectoryForExtension: Unit = {
     val pathToTheFile = (artifactPath in ScalaJSPlugin.autoImport.fullOptJS in Compile).value.toPath
     val targetDirectory = pathToTheFile.getParent.getParent
     val extensionDirectory = Paths.get(targetDirectory.toString, "unpacked_extension")
     IO.createDirectory(extensionDirectory.toFile)
+
+    val resourcesDirectory = Paths.get(targetDirectory.getParent.toString, "src", "main", "resources")
+    IO.copyDirectory(resourcesDirectory.toFile, extensionDirectory.toFile)
+    log.info("Resources copied.")
+
+    val directoryWithGeneratedJs = pathToTheFile.getParent
+    var stream: DirectoryStream[Path] = null
+    var jsFiles: Traversable[(File, File)] = null
+    try {
+      stream = Files.newDirectoryStream(directoryWithGeneratedJs, "*.js")
+      jsFiles = stream.map(file => (file.toFile,
+                                    Paths.get(extensionDirectory.toString, file.getFileName.toString).toFile))
+    } catch {
+      case ioException: IOException =>
+        log.error(s"Can't get stream for the $directoryWithGeneratedJs.")
+    } finally {
+      if (stream != null) {
+        stream.close()
+      }
+    }
+    IO.copy(jsFiles)
+    log.info("JS files copied.")
   }
 }
